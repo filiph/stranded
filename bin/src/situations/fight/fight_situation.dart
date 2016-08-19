75e49a7e87b06c65dc49d41ef9cbef8a6855c4b0
Filip Hracek <filip.hracek@gmail.com>
2016-08-19 15:24:54 -0700

    Improve the taking of turns in FightSituation

--- bin/src/situations/fight/fight_situation.dart
+++ bin/src/situations/fight/fight_situation.dart
@@ -2,10 +2,11 @@ library stranded.fight.fight_situation;
 
 import 'package:built_value/built_value.dart';
 import 'package:built_collection/built_collection.dart';
-
-import 'package:stranded/situation.dart';
-import 'package:stranded/actor.dart';
 import 'package:quiver/core.dart';
+
+import 'package:stranded/actor.dart';
+import 'package:stranded/situation.dart';
+import 'package:stranded/util/alternate_iterables.dart';
 
 part 'fight_situation.g.dart';
 
@@ -23,11 +24,10 @@ abstract class FightSituation extends SituationState
   Actor getActorAtTime(int i) {
     // TODO: add _lastActor and use that to offset [i] if needed (when one of
     //       the actors is removed during the fight.
-    if (i % 2 == 0) {
-      return playerTeam[(i ~/ 2) % playerTeam.length];
-    } else {
-      return enemyTeam[((i - 1) ~/ 2) % enemyTeam.length];
-    }
+    var allActors =
+        alternate/*<Actor>*/(playerTeam, enemyTeam).toList(growable: false);
+    i = i % allActors.length;
+    return allActors[i];
   }
 
   @override
