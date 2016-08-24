8475b916681965e3e68ea1547e9504ee0e3f4ab0
Filip Hracek <filip.hracek@gmail.com>
2016-08-23 23:12:01 -0700

    Remove files that have been moved to edgehead

--- bin/src/fight/pass.dart
+++
@@ -1,9 +0,0 @@
-import 'package:stranded/action.dart';
-import 'package:stranded/actor.dart';
-import 'package:stranded/world.dart';
-import 'package:stranded/storyline/storyline.dart';
-import 'off_balance_opportunity_situation.dart';
-
-var pass = new ClosureActorAction("Pass.", (_, __) => true, (a, w, s) {
-  return "${a.name} passes the opportunity";
-}, (_, __, ___) {}, 1.0);
